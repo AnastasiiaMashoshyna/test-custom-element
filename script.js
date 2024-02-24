60996cddf481d326ef7f7fed57f011ed2db9f9ec
Anastasiia Mashoshyna <anastasiiam@wix.com>
2024-02-24 21:41:16 +0200

    update script

--- script.js
+++ script.js
@@ -10,18 +10,20 @@ const DEBUG_TEXT = 'Loading the code for Custom Element \'wix-default-custom-ele
 const createImageContainer = () => {
   const imageContainer = document.createElement('div');
   imageContainer.id = 'wdce-image-container';
-  imageContainer.innerHTML = `<div class="card">ONE</div>
-<div class="card">TWO</div>
-<div class="card">THREE</div>
-<div class="card">FOUR</div>
-<div class="card">FIVE</div>
-<div class="card">SIX</div>
-<div class="card">SEVEN</div>
-<div class="card">EIGHT</div>
-<div class="card">NINE</div>
-<div class="card">TEN</div>
-<div class="card">ELEVEN</div>
-<div class="card">TWELVE</div>
+  imageContainer.innerHTML = `<div class="cards">
+  <div class="card">ONE</div>
+  <div class="card">TWO</div>
+  <div class="card">THREE</div>
+  <div class="card">FOUR</div>
+  <div class="card">FIVE</div>
+  <div class="card">SIX</div>
+  <div class="card">SEVEN</div>
+  <div class="card">EIGHT</div>
+  <div class="card">NINE</div>
+  <div class="card">TEN</div>
+  <div class="card">ELEVEN</div>
+  <div class="card">TWELVE</div>
+</div>
 `
   return imageContainer;
 };
